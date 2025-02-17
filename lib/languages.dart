6714aa1877f18e7fab4812063c384428fd998653
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-02-17 18:10:08 +0530

    setting up app

---
+++ lib/languages.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+class Languages extends Translations{
+
+  @override
+  Map<String , Map<String,String>> get keys =>{
+    'en_US':{
+      'message':'What is your name ',
+      'name': 'Kartik',
+      'localisation':'English',
+      'language':'Changing Tile Language'
+    },
+    'hi_IN':{
+      'message':'आपका क्या नाम है ',
+      'name': 'कार्तिक',
+      'localisation':'हिन्दी',
+      'language':'टाइल भाषा बदलना'
+
+    },
+  };
+
+}
---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
