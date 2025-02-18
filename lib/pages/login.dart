38ac697a9e5c68d1a326a7c316407a8672884163
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-02-18 18:14:45 +0530

    loginpage

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -40,156 +40,149 @@ class _LoginScreenState extends State<LoginScreen> {
 
 
 
-          SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:  EdgeInsets.only(top: Get.width*0.1),
-                    child: Text("Create",
-                        style: GoogleFonts.interTight(
-                          fontSize: Get.width*0.1,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.black,
-                          height: 1
-                        ),),
-                  ),
-                  Text("Your",
-                    style: GoogleFonts.interTight(
-                      fontSize: Get.width*0.1,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black,
-                      height: 1
-                    ),),
-                  Text("Account",
-                    style: GoogleFonts.interTight(
-                      fontSize: Get.width*0.1,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black,
-                      height: 1
-                    ),),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text("  And",
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:  EdgeInsets.only(top: Get.width*0.1),
+                  child: Text("Create",
                       style: GoogleFonts.interTight(
-                        fontSize: Get.width*0.05,
+                        fontSize: Get.width*0.1,
                         fontWeight: FontWeight.w800,
-                        color: Color.fromRGBO(255, 179, 0, 1),
-                        // height: 1
+                        color: Colors.black,
+                        height: 1
                       ),),
-                  ),
+                ),
+                Text("Your",
+                  style: GoogleFonts.interTight(
+                    fontSize: Get.width*0.1,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                    height: 1
+                  ),),
+                Text("Account",
+                  style: GoogleFonts.interTight(
+                    fontSize: Get.width*0.1,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                    height: 1
+                  ),),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: Text("  And",
+                    style: GoogleFonts.interTight(
+                      fontSize: Get.width*0.05,
+                      fontWeight: FontWeight.w800,
+                      color: Color.fromRGBO(255, 179, 0, 1),
+                      // height: 1
+                    ),),
+                ),
 
 
-                  SizedBox(height: 20),
+                SizedBox(height: 20),
 
-                  Padding(
-                    padding:  EdgeInsets.only(top: Get.height*0.05),
-                    child: _buildTextField("Username".tr, Icons.person),
-                  ),
-                  SizedBox(height: 12),
-                  _buildTextField("Email".tr, Icons.email),
-                  SizedBox(height: 12),
-                  _buildPasswordField("Password".tr, true),
-                  SizedBox(height: 12),
-                  _buildPasswordField("Confirm Password".tr, false),
+                Padding(
+                  padding:  EdgeInsets.only(top: Get.height*0.05),
+                  child: _buildTextField("Username".tr, Icons.person),
+                ),
+                SizedBox(height: 12),
+                _buildTextField("Email".tr, Icons.email),
+                SizedBox(height: 12),
+                _buildPasswordField("Password".tr, true),
+                SizedBox(height: 12),
+                _buildPasswordField("Confirm Password".tr, false),
 
 
-                  Row(
-                    children: [
-                      Checkbox(
-                  fillColor: WidgetStateProperty.resolveWith<Color>((Set<WidgetState> states) {
-          if (states.contains(WidgetState.selected)) {
-          return Color.fromRGBO(255, 179, 0, 1); // Checked color
-          }
-          return Color.fromRGBO(255, 179, 0, 1);}),
-                        value: isChecked,
-                        activeColor:Color.fromRGBO(255, 179, 0, 1),
-                        checkColor: Colors.black,
+                Row(
+                  children: [
+                    Checkbox(
+                fillColor: WidgetStateProperty.resolveWith<Color>((Set<WidgetState> states) {
+                    if (states.contains(WidgetState.selected)) {
+                    return Color.fromRGBO(255, 179, 0, 1); // Checked color
+                    }
+                    return Color.fromRGBO(255, 179, 0, 1);}),
+                      value: isChecked,
+                      activeColor:Color.fromRGBO(255, 179, 0, 1),
+                      checkColor: Colors.black,
 
-                        onChanged: (value) {
-                          setState(() {
-                            isChecked = value!;
-                          });
-                        },
-                      ),
-                      Text(
-                        "I agree to the Terms & Conditions".tr,
-                        style: GoogleFonts.poppins(fontSize: 14),
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 10),
-                  Center(
-                    child: SizedBox(
-                      width: Get.width*0.6,
-                      height: Get.width*0.1,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromRGBO(0,113,220,1.0,),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: Text(
-                          "Sign Up",
-                          style: GoogleFonts.poppins(
-                            fontSize: Get.width*0.045,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
                     ),
-                  ),
+                    Text(
+                      "I agree to the Terms & Conditions".tr,
+                      style: GoogleFonts.poppins(fontSize: 14),
+                    ),
+                  ],
+                ),
 
-
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 1,
-                        color: Colors.grey,
-                      ),
-                      SizedBox(width: 8),
-                      Text("or", style: GoogleFonts.poppins(fontSize: 16)),
-                      SizedBox(width: 8),
-                      Container(
-                        width: 100,
-                        height: 1,
-                        color: Colors.grey,
-                      ),
-                    ],
-                  ),
-
-
-                  SizedBox(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: OutlinedButton.icon(
+                SizedBox(height: 10),
+                Center(
+                  child: SizedBox(
+                    width: Get.width*0.65,
+                    height: Get.width*0.11,
+                    child: ElevatedButton(
                       onPressed: () {},
-                      icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
-                      label: Text(
-                        "Continue with Google",
-                        style: GoogleFonts.poppins(fontSize: 16, color: Colors.black),
-                      ),
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(color: Colors.grey),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 8,
+                        backgroundColor: Color.fromRGBO(0,113,220,1.0,),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30),
                         ),
                       ),
+                      child: Text(
+                        "Sign Up",
+                        style: GoogleFonts.poppins(
+                          fontSize: Get.width*0.045,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
+                ),
 
-                  SizedBox(height: 16),
-                  Center(
+
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    // SizedBox(width: 8),
+                    Text("or", style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.grey)),
+
+                  ],
+                ),
+
+
+                SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: Image.asset('assets/images/auth/google.png',height: 32,),
+                    label: Text(
+                      "Continue with Google",
+                      style: GoogleFonts.poppins(fontSize: 16, color: Colors.black),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.grey),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // SizedBox(height: 30),
+                Padding(
+                  padding:  EdgeInsets.only(top: Get.width*0.07),
+                  child: Center(
                     child: Text.rich(
                       TextSpan(
                         text: "Already have an account? ",
@@ -199,16 +192,16 @@ class _LoginScreenState extends State<LoginScreen> {
                             text: "Login",
                             style: GoogleFonts.poppins(
                               fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue,
+                              fontWeight: FontWeight.w500,
+                              color: Color.fromRGBO(255,179,0,1),
                             ),
                           ),
                         ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
