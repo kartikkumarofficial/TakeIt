1624f1bc59f800e11351819547cb673dde8e2308
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-02-21 18:59:24 +0530

    dashboard screen

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -153,7 +153,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   Padding(
                     padding: EdgeInsets.only(left: Get.width*0.035),
                     child: Container(
-                      height: Get.width*0.351,
+                      height: Get.width*0.346,
                       width: Get.width*0.3,
                       decoration: BoxDecoration(
                           color: Color.fromRGBO(216, 235, 252, 1),
@@ -187,7 +187,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                             child: Column(
                               children: [
                               Text('Tv & Laptop',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
-                                fontSize: Get.width*0.04,
+                                fontSize: Get.width*0.035,fontWeight: FontWeight.w400
 
                               ),),
                               Text('From \$100/M',
@@ -208,7 +208,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   Padding(
                     padding: EdgeInsets.only(left: Get.width*0.023),
                     child: Container(
-                      height: Get.width*0.351,
+                      height: Get.width*0.346,
                       width: Get.width*0.3,
                       decoration: BoxDecoration(
                           color: Color.fromRGBO(216, 235, 252, 1),
@@ -246,7 +246,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                 child: Column(
                                   children: [
                                     Text('Latest Mobile',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
-                                      fontSize: Get.width*0.04,
+                                        fontSize: Get.width*0.035,fontWeight: FontWeight.w400
 
                                     ),),
                                     Text('From \$60/M',
@@ -272,7 +272,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   Padding(
                     padding: EdgeInsets.only(left: Get.width*0.023),
                     child: Container(
-                      height: Get.width*0.351,
+                      height: Get.width*0.346,
                       width: Get.width*0.28,
                       decoration: BoxDecoration(
                           color: Color.fromRGBO(216, 235, 252, 1),
@@ -311,14 +311,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                 width: double.infinity,
                                 child: Column(
                                   children: [
-                                    Text('Beauty Products',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
-                                      fontSize: Get.width*0.04,
+                                    Padding(
+                                      padding:  EdgeInsets.symmetric(horizontal: 2),
+                                      child: Text('Beauty Products',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                          fontSize: Get.width*0.035,fontWeight: FontWeight.w400
 
-                                    ),),
-                                    Text('From \$9/M',
+                                      ),),
+                                    ),
+                                    Text('Take it at\$9/M',
                                       overflow: TextOverflow.ellipsis,
                                       style: GoogleFonts.inter(
-                                          fontSize: Get.width*0.04,
+                                          fontSize: Get.width*0.038,
                                           fontWeight: FontWeight.w500
                                       ),),
                                   ]
@@ -668,7 +671,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                   child: Column(
                                     children: [
                                       Padding(
-                                        padding:  EdgeInsets.only(bottom: 8.0),
+                                        padding:  EdgeInsets.only(bottom: 5.0),
                                         child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
                                           fontWeight: FontWeight.w600,
                                           fontSize: Get.width*0.03,
@@ -711,13 +714,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         child: Stack(
                           children: [
                             Positioned(
-                              left: -Get.width*0.015,
+                              left: -Get.width*0.001,
                               // right: 0.02,
-                              top: 0,
-                              bottom: -Get.width*0.08,
-                              child: Image.asset('assets/images/homescreen/gifts.png',
+                              // top: 0,
+                              bottom: -Get.width*0.07,
+                              child: Image.asset('assets/images/gifts.png',
                                 // fit: BoxFit.fill,
-                                width: Get.width*0.32,),),
+                                width: Get.width*0.3,),),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: [
@@ -741,8 +744,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                   child: Column(
                                     children: [
                                       Padding(
-                                        padding:  EdgeInsets.only(bottom: 8.0),
-                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                        padding:  EdgeInsets.only(bottom: 5.0),
+                                        child: Text('Gifts',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
                                           fontWeight: FontWeight.w600,
                                           fontSize: Get.width*0.03,
 
@@ -784,13 +787,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         child: Stack(
                           children: [
                             Positioned(
-                              left: -Get.width*0.015,
+                              left: -Get.width*0.001,
                               // right: 0.02,
-                              top: 0,
-                              bottom: -Get.width*0.08,
-                              child: Image.asset('assets/images/homescreen/kids.png',
+                              // top: 0,
+                              bottom: -Get.width*0.07,
+                              child: Image.asset('assets/images/watch.png',
                                 // fit: BoxFit.fill,
-                                width: Get.width*0.32,),),
+                                width: Get.width*0.27,),),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: [
@@ -814,8 +817,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                   child: Column(
                                     children: [
                                       Padding(
-                                        padding:  EdgeInsets.only(bottom: 8.0),
-                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                        padding:  EdgeInsets.only(bottom: 5.0),
+                                        child: Text('Men\'s Watches',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
                                           fontWeight: FontWeight.w600,
                                           fontSize: Get.width*0.03,
 
@@ -857,13 +860,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         child: Stack(
                           children: [
                             Positioned(
-                              left: -Get.width*0.015,
+                              left: -Get.width*0.001,
                               // right: 0.02,
-                              top: 0,
+                              // top: 0,
                               bottom: -Get.width*0.08,
-                              child: Image.asset('assets/images/homescreen/kids.png',
+                              child: Image.asset('assets/images/homescreen/vaseline.png',
                                 // fit: BoxFit.fill,
-                                width: Get.width*0.32,),),
+                                width: Get.width*0.3,),),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: [
@@ -887,8 +890,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                   child: Column(
                                     children: [
                                       Padding(
-                                        padding:  EdgeInsets.only(bottom: 8.0),
-                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                        padding:  EdgeInsets.only(bottom: 5.0),
+                                        child: Text('Body Lotion',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
                                           fontWeight: FontWeight.w600,
                                           fontSize: Get.width*0.03,
 
@@ -909,79 +912,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: Get.width*0.034),
-                      child: Container(
 
-                        height: Get.width*0.35,
-                        width: Get.width*0.27,
-                        decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                spreadRadius: 0.1,
-                                blurRadius: 2,
-
-                              ),],
-                            color: Color.fromRGBO(245, 245, 245, 1),
-
-                            borderRadius: BorderRadius.circular(10)
-                        ),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: -Get.width*0.015,
-                              // right: 0.02,
-                              top: 0,
-                              bottom: -Get.width*0.08,
-                              child: Image.asset('assets/images/homescreen/kids.png',
-                                // fit: BoxFit.fill,
-                                width: Get.width*0.32,),),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Container(
-                                  // color: Colors.red,
-                                  child: Padding(
-                                    padding: EdgeInsets.only(left: 2,top: 15),
-                                  ),
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(2),
-                                        topRight: Radius.circular(2),
-                                        bottomLeft: Radius.circular(10),
-                                        bottomRight: Radius.circular(10)
-                                    ),
-                                    color: Colors.white,
-                                  ),
-                                  width: double.infinity,
-                                  child: Column(
-                                    children: [
-                                      Padding(
-                                        padding:  EdgeInsets.only(bottom: 8.0),
-                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
-                                          fontWeight: FontWeight.w600,
-                                          fontSize: Get.width*0.03,
-
-                                        ),),
-                                      ),
-
-                                    ]
-                                    ,)
-                                  ,)
-
-                              ],
-
-                            ),
-
-
-
-                          ],
-                        ),
-                      ),
-                    ),
 
 
 
