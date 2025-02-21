504c5edbdfd36609ff831dd6f0f5071e9abc466f
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-02-21 18:49:29 +0530

    dashboard screen

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -627,9 +627,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
                           boxShadow: [
                             BoxShadow(
                             color: Colors.black.withOpacity(0.2),
-                            spreadRadius: 0.1, // Spread radius
-                            blurRadius: 2, // Blur radius
-                            // offset: Offset(1, 1), // Offset in x and y direction
+                            spreadRadius: 0.1,
+                            blurRadius: 2,
+
                           ),],
                             color: Color.fromRGBO(245, 245, 245, 1),
 
@@ -638,8 +638,300 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         child: Stack(
                           children: [
                             Positioned(
-                              // left: Get.width*0.01,
-                              // right: 1,
+                              left: -Get.width*0.015,
+                              // right: 0.02,
+                              top: 0,
+                              bottom: -Get.width*0.08,
+                              child: Image.asset('assets/images/homescreen/kids.png',
+                                // fit: BoxFit.fill,
+                                width: Get.width*0.32,),),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  // color: Colors.red,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 2,top: 15),
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(2),
+                                        topRight: Radius.circular(2),
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight: Radius.circular(10)
+                                    ),
+                                    color: Colors.white,
+                                  ),
+                                  width: double.infinity,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:  EdgeInsets.only(bottom: 8.0),
+                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: Get.width*0.03,
+
+                                        ),),
+                                      ),
+
+                                    ]
+                                    ,)
+                                  ,)
+
+                              ],
+
+                            ),
+
+
+
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: Get.width*0.034),
+                      child: Container(
+
+                        height: Get.width*0.35,
+                        width: Get.width*0.27,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 0.1,
+                                blurRadius: 2,
+
+                              ),],
+                            color: Color.fromRGBO(245, 245, 245, 1),
+
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: -Get.width*0.015,
+                              // right: 0.02,
+                              top: 0,
+                              bottom: -Get.width*0.08,
+                              child: Image.asset('assets/images/homescreen/gifts.png',
+                                // fit: BoxFit.fill,
+                                width: Get.width*0.32,),),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  // color: Colors.red,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 2,top: 15),
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(2),
+                                        topRight: Radius.circular(2),
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight: Radius.circular(10)
+                                    ),
+                                    color: Colors.white,
+                                  ),
+                                  width: double.infinity,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:  EdgeInsets.only(bottom: 8.0),
+                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: Get.width*0.03,
+
+                                        ),),
+                                      ),
+
+                                    ]
+                                    ,)
+                                  ,)
+
+                              ],
+
+                            ),
+
+
+
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: Get.width*0.034),
+                      child: Container(
+
+                        height: Get.width*0.35,
+                        width: Get.width*0.27,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 0.1,
+                                blurRadius: 2,
+
+                              ),],
+                            color: Color.fromRGBO(245, 245, 245, 1),
+
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: -Get.width*0.015,
+                              // right: 0.02,
+                              top: 0,
+                              bottom: -Get.width*0.08,
+                              child: Image.asset('assets/images/homescreen/kids.png',
+                                // fit: BoxFit.fill,
+                                width: Get.width*0.32,),),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  // color: Colors.red,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 2,top: 15),
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(2),
+                                        topRight: Radius.circular(2),
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight: Radius.circular(10)
+                                    ),
+                                    color: Colors.white,
+                                  ),
+                                  width: double.infinity,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:  EdgeInsets.only(bottom: 8.0),
+                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: Get.width*0.03,
+
+                                        ),),
+                                      ),
+
+                                    ]
+                                    ,)
+                                  ,)
+
+                              ],
+
+                            ),
+
+
+
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: Get.width*0.034),
+                      child: Container(
+
+                        height: Get.width*0.35,
+                        width: Get.width*0.27,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 0.1,
+                                blurRadius: 2,
+
+                              ),],
+                            color: Color.fromRGBO(245, 245, 245, 1),
+
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: -Get.width*0.015,
+                              // right: 0.02,
+                              top: 0,
+                              bottom: -Get.width*0.08,
+                              child: Image.asset('assets/images/homescreen/kids.png',
+                                // fit: BoxFit.fill,
+                                width: Get.width*0.32,),),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  // color: Colors.red,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 2,top: 15),
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(2),
+                                        topRight: Radius.circular(2),
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight: Radius.circular(10)
+                                    ),
+                                    color: Colors.white,
+                                  ),
+                                  width: double.infinity,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:  EdgeInsets.only(bottom: 8.0),
+                                        child: Text('Kids Clothing',overflow: TextOverflow.ellipsis,style: GoogleFonts.inter(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: Get.width*0.03,
+
+                                        ),),
+                                      ),
+
+                                    ]
+                                    ,)
+                                  ,)
+
+                              ],
+
+                            ),
+
+
+
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: Get.width*0.034),
+                      child: Container(
+
+                        height: Get.width*0.35,
+                        width: Get.width*0.27,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 0.1,
+                                blurRadius: 2,
+
+                              ),],
+                            color: Color.fromRGBO(245, 245, 245, 1),
+
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: -Get.width*0.015,
+                              // right: 0.02,
                               top: 0,
                               bottom: -Get.width*0.08,
                               child: Image.asset('assets/images/homescreen/kids.png',
